8576c3e99fd82357f554311584c4745e35f8ef6b
AGrant324 <AGrant324@users.noreply.github.com>
2017-04-28 15:57:47 +0100

    Update mainPage.php

--- studentHelper/mainPage.php
+++ studentHelper/mainPage.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
+//Starts a php session so that if one hasn't already been started, there isn't an issue caused by calling a session variable
 if(!$_SESSION['loggedIn']) {
-	header('location: index.php');
+    //If the user attempts to access this page without being logged in, they will be redirected to the login page
+    header('location: index.php');
 }
 ?>
 <!DOCTYPE html>
@@ -10,16 +12,13 @@ if(!$_SESSION['loggedIn']) {
 	<meta charset="UTF-8">
 	<title>Main Page</title>
 	<link href="stylesheet.css" rel="stylesheet" type="text/css">
-	<script src="javascript.js">
-	</script>
 </head>
 <body>
 	<section id="externalWrapper">
-		<h2 style="display:none;">.</h2>
 		<nav>
 			<ul>
 				<li>
-					<a class="active" href="mainPage.php" id="logo">Student Helper</a>
+					<a class="active" href="mainPage.php" id="logo">Student Helper</a> <!--The anchor with the class of active will be white rather than grey to signify that it is the current page-->
 				</li>
 				<li>
 					<a href="createGroupPage.php">Create Group</a>
@@ -38,53 +37,33 @@ if(!$_SESSION['loggedIn']) {
 				</li>
 			</ul>
 		</nav>
-
 		<section id="internalWrapper">
-	 		<div id="wrapper">
-
-				   <ul class="rig columns-3">
-		<li>
-			<img src="St_Images/routeFinderExample.gif" alt="" />
-			<h3> RouteFinder </h3>
-
-			<p>Find your way around the University</p>
-
-		</li>
-		<li>
-			<img src="St_Images/findStudyGroup.gif" alt="" />
-			<h3>Look up study groups</h3>
-			<p>Find groups that you can study with</p>
-
-		</li>
-		<li>
-			<img src="St_Images/createStudyGroup.gif" alt="" />
-			<h3>Create a study group</h3>
-			<p>Create a group that is specific to you</p>
-
-		</li>
-		<li>
-			<img src="St_Images/dropOfExample.gif" alt=""/>
-			<h3>Find the course work drop off point</h3>
-			<p>Look up what course work drop off point are located</p>
-
-		</li>
-
-	</ul>
-
-	</div>
-
-
-
+			<div class="row cf">
+				<div class="third">
+					<a href="routeFinder.php"><img alt="" src="St_Images/routeFinderExample.gif">
+					<h3>RouteFinder</h3>
+					<p>Find your way around the University</p></a>
+				</div>
+				<div class="third">
+					<a href="joinGroup.php"><img alt="" src="St_Images/findStudyGroup.gif">
+					<h3>Look up study groups</h3>
+					<p>Find groups that you can study with</p></a>
+				</div>
+				<div class="third">
+					<a href="createGroupPage.php"><img alt="" src="St_Images/createStudyGroup.gif">
+					<h3>Create a study group</h3>
+					<p>Create a group that is specific to you</p></a>
+				</div>
+				<div class="third">
+					<a href="courseworkDropoffStud.php"><img alt="" src="St_Images/dropOfExample.gif">
+					<h3>Find the course work drop off point</h3>
+					<p>Look up what course work drop off point are located</p></a>
+				</div>
+			</div>
 		</section>
-
 	</section>
 	<footer>
-				<a href="http://validator.w3.org/check?uri=referer">Valid HTML 5</a>
-				<a href="http://jigsaw.w3.org/css-validator/check/referer">
-					<img style="border:0;width:88px;height:31px"
-						src="http://jigsaw.w3.org/css-validator/images/vcss"
-						alt="Valid CSS!" />
-				</a>
-			</footer>
+		<a href="http://validator.w3.org/check?uri=referer">Valid HTML 5</a> <a href="http://jigsaw.w3.org/css-validator/check/referer"><img alt="Valid CSS!" src="http://jigsaw.w3.org/css-validator/images/vcss" style="border:0;width:88px;height:31px"></a>
+	</footer>
 </body>
 </html>
